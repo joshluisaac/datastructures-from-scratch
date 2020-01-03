ab8771127b225f7d2dc267cdc139547fb73a323a
Joshua Uzo <nwankwo.joshua@gmail.com>
2020-01-03 16:36:34 +1100

    Added tests

--- src/test/java/au/com/avantsystems/SimpleQueueBasedStackTest.java
+++ src/test/java/au/com/avantsystems/SimpleQueueBasedStackTest.java
@@ -1,11 +1,41 @@
 package au.com.avantsystems;
 
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class SimpleQueueBasedStackTest {
 
-    private Stack<String> stack = new SimpleQueueBasedStack();
+    private Stack<String> stack = new SimpleQueueBasedStack<>();
 
+    @Test
     void shouldReturn100_WhenStackIsPopped(){
+        stack.push("100");
+        stack.push("200");
+        stack.push("300");
+        stack.push("400");
+        stack.push("900");
+        assertThat(stack.pop()).isEqualTo("900");
+    }
 
+    @Test
+    void shouldReturn5_AsStackSize(){
+        stack.push("100");
+        stack.push("200");
+        stack.push("300");
+        stack.push("400");
+        stack.push("900");
+        assertThat(stack.size()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldReturn4_AsStackSize(){
+        stack.push("100");
+        stack.push("200");
+        stack.push("300");
+        stack.push("400");
+        stack.push("900");
+        stack.pop();
+        assertThat(stack.size()).isEqualTo(4);
     }
 
 }
